b7b93bac330c021301c28990440da733422f6b70
Starassassin-github <77239277+Starassassin-github@users.noreply.github.com>
2022-07-18 13:38:31 +0700

    02-EjectingToTheBareWorkflow
    02-EjectingToTheBareWorkflow

---
+++ components/ExpensesOutput/ExpensesList.js
@@ -0,0 +1,20 @@
+import { FlatList } from "react-native";
+
+import ExpensesItem from "./ExpensesItem";
+
+function renderExpensesItem(itemData) {
+    return (
+        <ExpensesItem {...itemData.item} />
+    )
+}
+
+function ExpensesList({ expenses }) {
+    return (
+        <FlatList
+            data={expenses}
+            renderItem={renderExpensesItem}
+            keyExtractor={(item) => item.id}
+        />
+    )
+}
+export default ExpensesList;
